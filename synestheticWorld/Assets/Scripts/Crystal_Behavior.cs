31973403b7f6f2f300aebb853f66f0b255abcf1a
Sam Jianghao Hu <jh4828@nyu.edu>
2018-02-25 22:45:00 -0500

    Added crystal fade in

--- synestheticWorld/Assets/Scripts/Crystal_Behavior.cs
+++ synestheticWorld/Assets/Scripts/Crystal_Behavior.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Crystal_Behavior : MonoBehaviour {
+	bool ifAbsorbing = false;
 	float alpha = 1f;
 	SpriteRenderer mySpriteRender;
 
@@ -15,19 +16,36 @@ public class Crystal_Behavior : MonoBehaviour {
 	void Update () {
 		mySpriteRender.color = new Color (mySpriteRender.color.r, mySpriteRender.color.g, mySpriteRender.color.b, alpha);
 
+		if (ifAbsorbing == false && alpha < 1f) {
+			FadeIn ();
+		}
+
 		if ((alpha - 0) <= 0.01f) {
 			Player_AbilityStack.AddAbility (this.gameObject);
 			Destroy (this.gameObject);
 		}
+
+		Debug.Log (ifAbsorbing);
 	}
 
 	void FadeOut(){
 		alpha -= 0.015f;
 	}
 
+	void FadeIn(){
+		alpha += 0.015f;
+	}
+
 	void OnTriggerStay2D(Collider2D other){
 		if (other.tag == "Player") {
+			ifAbsorbing = true;
 			FadeOut ();
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other){
+		if (other.tag == "Player") {
+			ifAbsorbing = false;
 		}
 	}
 }
