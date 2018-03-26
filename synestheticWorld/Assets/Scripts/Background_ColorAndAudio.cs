f6c280db8fb487ec69baabab55beb3dedc136c45
Sam Jianghao Hu <jh4828@nyu.edu>
2018-03-26 03:26:21 -0400

    Added Background music

--- synestheticWorld/Assets/Scripts/Background_ColorAndAudio.cs
+++ synestheticWorld/Assets/Scripts/Background_ColorAndAudio.cs
@@ -5,28 +5,35 @@ using UnityEngine;
 public class Background_ColorAndAudio : MonoBehaviour {
 	public Color color0;
 	public Color match0;
+	public AudioClip clip0;
 	
 	public Color color1;
 	public Color match1;
+	public AudioClip clip1;
 	
 	public Color color2;
 	public Color match2;
+	public AudioClip clip2;
 	
 	public Color color3;
 	public Color match3;
+	public AudioClip clip3;
 
 	public GameObject camera;
 	public GameObject player;
 	public GameObject moveBG;
 
 	Camera cam;
+	AudioSource bgmPlayer;
 	Dictionary<Color, Color> colorDict = new Dictionary<Color, Color> ();
+	Dictionary<Color, AudioClip> audioDict = new Dictionary<Color, AudioClip> ();
 	SpriteRenderer mySpriteRenderer;
 	SpriteRenderer playerSprite;
 
 	// Use this for initialization
 	void Start () {
 		cam = camera.GetComponent<Camera> ();
+		bgmPlayer = transform.parent.GetComponent<AudioSource> ();
 		mySpriteRenderer = GetComponent<SpriteRenderer> ();
 		playerSprite = player.GetComponent<SpriteRenderer> ();
 
@@ -36,6 +43,7 @@ public class Background_ColorAndAudio : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		ChangeBGColor ();
+		ChangeBGM ();
 	}
 
 	void InitializeDict(){
@@ -43,6 +51,11 @@ public class Background_ColorAndAudio : MonoBehaviour {
 		colorDict.Add (color1, match1);
 		colorDict.Add (color2, match2);
 		colorDict.Add (color3, match3);
+
+		audioDict.Add (color0, clip0);
+		audioDict.Add (color1, clip1);
+		audioDict.Add (color2, clip2);
+		audioDict.Add (color3, clip3);
 	}
 		
 	void ChangeBGColor(){
@@ -54,6 +67,23 @@ public class Background_ColorAndAudio : MonoBehaviour {
 			matchedColor = new Color (matchedColor.r, matchedColor.g, matchedColor.b, 255);
 			mySpriteRenderer.color = matchedColor;
 			cam.backgroundColor = matchedColor;
+		}
+	}
+
+	void ChangeBGM(){
+		if (Stage_Utilities.compareColorsLoose (playerSprite.color, Color.red) == false) {
+			
+			AudioClip bgmClip;
+			audioDict.TryGetValue (playerSprite.color, out bgmClip);
+
+			if (bgmPlayer.clip != bgmClip) {
+				bgmPlayer.clip = bgmClip;
+				bgmPlayer.loop = true;
+
+				if (bgmPlayer.isPlaying == false) {
+					bgmPlayer.Play ();
+				}
+			}
 		}
 	}
 }
--- synestheticWorld/Assets/Scripts/Player_SoundControl.cs
+++ synestheticWorld/Assets/Scripts/Player_SoundControl.cs
@@ -18,23 +18,25 @@ public class Player_SoundControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if ((Player_Control.IfStunned () == false) && (myRigidbody.velocity.x != 0) && (myRigidbody.velocity.y == 0)) {
-			movePlayer.clip = walk;
-			movePlayer.loop = true;
+		if (myRigidbody != null) {
+			if ((Player_Control.IfStunned () == false) && (myRigidbody.velocity.x != 0) && (myRigidbody.velocity.y == 0)) {
+				movePlayer.clip = walk;
+				movePlayer.loop = true;
 
-			if (movePlayer.isPlaying == false) {
+				if (movePlayer.isPlaying == false) {
+					movePlayer.Play ();
+				}
+			} else {
+				if ((movePlayer.clip == walk) && movePlayer.isPlaying) {
+					movePlayer.Stop ();
+				}
+			}
+
+			if ((Input.GetKeyDown (KeyCode.Space)) && (myRigidbody.velocity.y == 0)) {
+				movePlayer.clip = jump;
+				movePlayer.loop = false;
 				movePlayer.Play ();
 			}
-		} else {
-			if ((movePlayer.clip == walk) && movePlayer.isPlaying) {
-				movePlayer.Stop ();
-			}
-		}
-
-		if ((Input.GetKeyDown (KeyCode.Space)) && (myRigidbody.velocity.y == 0)) {
-			movePlayer.clip = jump;
-			movePlayer.loop = false;
-			movePlayer.Play ();
 		}
 	}
 }
