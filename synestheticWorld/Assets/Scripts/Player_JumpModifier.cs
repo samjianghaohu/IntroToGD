de429a1fcac9194ada2eb10cb6657911739b557e
Sam Jianghao Hu <jh4828@nyu.edu>
2018-04-03 16:30:17 -0400

    Optimized jump modifier

--- synestheticWorld/Assets/Scripts/Player_JumpModifier.cs
+++ synestheticWorld/Assets/Scripts/Player_JumpModifier.cs
@@ -2,23 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player_JumpModifier : MonoBehaviour {
+public class Player_JumpModifier : MonoBehaviour {//This scripts modifies jump
+
+	//Different modifiers used in different jumps (tap/hold)
 	public float fallMultiplier;
 	public float lowJumpMultiplier;
 
+
 	Rigidbody2D myrigidbody;
+
+
 	// Use this for initialization
 	void Start () {
 		myrigidbody = GetComponent<Rigidbody2D> ();
 	}
-	
+
+
 	// Update is called once per frame
 	void FixedUpdate () {
 		if (myrigidbody != null) {
-			if (myrigidbody.velocity.y < 0) {
-				myrigidbody.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
-			} else if (myrigidbody.velocity.y > 0 && !Input.GetKey (KeyCode.Space)) {
-				myrigidbody.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+			if (myrigidbody.velocity.y < 0) {//fall slower/jump higher when you hold jump key
+				myrigidbody.gravityScale = fallMultiplier;
+			} else if (myrigidbody.velocity.y > 0 && !Input.GetKey (KeyCode.Space)) {//fall faster when you tap jump key
+				myrigidbody.gravityScale = lowJumpMultiplier;
+			} else {
+				myrigidbody.gravityScale = 1f;
 			}
 		}
 	}
