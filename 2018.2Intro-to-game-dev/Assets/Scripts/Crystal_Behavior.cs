28679586a7a28e8ac236fd35ead0fd7ddaeb0264
Sam Jianghao Hu <jh4828@nyu.edu>
2018-04-23 01:01:33 -0400

    Added sound effect when get crystals

--- 2018.2Intro-to-game-dev/Assets/Scripts/Crystal_Behavior.cs
+++ 2018.2Intro-to-game-dev/Assets/Scripts/Crystal_Behavior.cs
@@ -4,21 +4,26 @@ using UnityEngine;
 
 public class Crystal_Behavior : MonoBehaviour {//This script controls crystal behavior
 
+	//Declare absorb particle effect and sound clip
 	public GameObject absorbPrefab;
+	public AudioClip getClip;
 
 	bool ifAbsorbing = false;
+	bool isAbsorbed = false;
 	float alpha = 1f;
 
 
 	GameObject absorbObject;
 	GameObject player;
 	SpriteRenderer mySpriteRender;
+	AudioSource myAudio;
 
 
 	// Use this for initialization
 	void Start () {
 		player = GameObject.FindWithTag ("Player");
 		mySpriteRender = GetComponent<SpriteRenderer> ();
+		myAudio = GetComponentInParent<AudioSource> ();
 	}
 
 
@@ -27,26 +32,45 @@ public class Crystal_Behavior : MonoBehaviour {//This script controls crystal be
 		mySpriteRender.color = new Color (mySpriteRender.color.r, mySpriteRender.color.g, mySpriteRender.color.b, alpha);
 
 
-		//The crystal fades out and triggers absorbing effects when player touches it
-		if (ifAbsorbing) {
-			FadeOut ();
+		if (!isAbsorbed) {
+
+			//The crystal fades out and triggers absorbing effects when player touches it
+			if (ifAbsorbing) {
+				FadeOut ();
 
 	
-			//Update the position of absorb effect with player's position
-			absorbObject.transform.position = player.transform.position;
-		}
+				//Update the position of absorb effect with player's position
+				absorbObject.transform.position = player.transform.position;
+			}
 
 
-		//If player leaves the crystal before it's completely absorbed, the crystal fades back in
-		if (!ifAbsorbing && alpha < 1f) {
-			FadeIn ();
-		}
+			//If player leaves the crystal before it's completely absorbed, the crystal fades back in
+			if (!ifAbsorbing && alpha < 1f) {
+				FadeIn ();
+			}
 
 
-		//Destroy the crystal when it's completely absorbed by player
-		if ((alpha - 0) <= 0.01f) {
-			player.GetComponent<Player_AbilityStack>().AddAbility (this.gameObject);
-			Destroy (this.gameObject);
+			//Play "get crystal" sound effect and change state when it's completely absorbed by player
+			if ((alpha - 0) <= 0.01f) {
+
+				//Destory absorb effect if there's one
+				Destroy (absorbObject);
+
+
+				//Play "get" sound effect
+				player.GetComponent<Player_AbilityStack> ().AddAbility (this.gameObject);
+				myAudio.clip = getClip;
+				myAudio.loop = false;
+				myAudio.Play ();
+				isAbsorbed = true;
+
+			}
+		} else {
+
+			//Destroy the crystal after the get sound is played
+			if (!myAudio.isPlaying) {
+				Destroy (this.gameObject);
+			}
 		}
 				
 	}
@@ -63,7 +87,7 @@ public class Crystal_Behavior : MonoBehaviour {//This script controls crystal be
 
 
 	void OnTriggerStay2D(Collider2D other){
-		if (other.tag == "Player") {
+		if (!isAbsorbed && (other.tag == "Player")) {
 
 
 			//Instantiate absorb particle effects if player just starts to touch the crystal
@@ -81,7 +105,7 @@ public class Crystal_Behavior : MonoBehaviour {//This script controls crystal be
 
 
 	void OnTriggerExit2D(Collider2D other){
-		if (other.tag == "Player") {
+		if (!isAbsorbed && (other.tag == "Player")) {
 			Destroy (absorbObject);
 			ifAbsorbing = false;
 		}
