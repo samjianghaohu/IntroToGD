cf7e4d0d8509cb514b3e0f9ce4610e5cb545a60f
Sam Jianghao Hu <jh4828@nyu.edu>
2018-04-23 23:37:06 -0400

    Added title screen music and background effect

---
+++ 2018.2Intro-to-game-dev/Assets/Scripts/Background_MovingColorReverse.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background_MovingColorReverse : MonoBehaviour {//This script moves the color block behind the background image.
+
+	public float movingSpeed;
+	public GameObject camera;
+
+
+	// Use this for initialization
+	void Start () {
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x <= (camera.transform.position.x - 12)) {
+			transform.position = new Vector3 ((camera.transform.position.x + 12), transform.position.y, transform.position.z);
+		} else {
+			transform.position += movingSpeed * Vector3.right;
+		}
+	}
+}
